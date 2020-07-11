754e23ce8699a7cb54f30d8792647507c23b6495
xiaowei <xiaowei_wang@aliyun.com>
2020-07-11 20:38:52 +0800

    init:mall project init

---
+++ mall-order/src/main/java/io/xiaowei/order/MallOrderApplication.java
@@ -0,0 +1,13 @@
+package io.xiaowei.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class, args);
+    }
+
+}
